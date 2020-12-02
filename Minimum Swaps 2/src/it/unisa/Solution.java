f4d46e184fc8d2e5d5e0e78fc88edb60333a8083
Antonio Cirillo <sig.cirillo@gmail.com>
2020-12-02 11:54:22 +0100

    Minimum Swaps 2

---
+++ Minimum Swaps 2/src/it/unisa/Solution.java
@@ -0,0 +1,60 @@
+package it.unisa;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+
+        int moves = 0;
+        int y;
+
+        for(int i = 0; i < arr.length - 1; i++) {
+            if(arr[i] != (i + 1)) {
+                for(y = i + 1; arr[y] != (i + 1); y++) ;
+                int tmp = arr[i];
+                arr[i] = arr[y];
+                arr[y] = tmp;
+                moves++;
+            }
+        }
+
+        return moves;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
